b2a7015658df7ea85df3fca9c302f68315c84153
Ansyar Hafid <ansyar@anchorock.com>
2024-06-11 17:42:20 +0700

    Bundle extension fixes

--- Sources/FXCoreUI/Extensions/Bundle+Extension.swift
+++ Sources/FXCoreUI/Extensions/Bundle+Extension.swift
@@ -8,17 +8,22 @@
 import Foundation
 
 public extension Bundle {
+    // Function to get the bundle for a specific class
     static func classBundle(for class: AnyClass) -> Bundle {
         return Bundle(for: `class`.self)
     }
     
+    // Computed property to get the module bundle
     static var moduleBundle: Bundle = {
         let bundleName = "FXCoreUI"
         
+        // Candidates for the bundle URL
         let candidates = [
             Bundle.main.resourceURL,
             Bundle(for: BaseViewController.self).resourceURL,
             Bundle.main.bundleURL,
+            // This is an important addition for SPM
+            Bundle.module.resourceURL
         ]
         
         for candidate in candidates {
@@ -26,6 +31,11 @@ public extension Bundle {
             if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
                 return bundle
             }
+        }
+        
+        // Fallback: Trying to find the bundle within the module itself (SPM case)
+        if let bundle = Bundle.module.url(forResource: bundleName, withExtension: "bundle").flatMap(Bundle.init(url:)) {
+            return bundle
         }
         
         fatalError("unable to find bundle named \(bundleName)")
